75d1a63e2821d97475b248ee1c61763f0c1768cd
Vignesh Chandrasekharan <vchan444@gmail.com>
2018-09-16 23:10:56 -0500

    drawerwidth is 300 now

--- App.js
+++ App.js
@@ -105,7 +105,7 @@ export const DrawerNavigator = createDrawerNavigator({
 }, {
     //initialRouteName: 'HomeFeedStack',
     contentComponent: DrawerScreen,
-    drawerWidth: 200
+    drawerWidth: 300
   });
 
 const MenuImage = ({ navigation }) => {
