f8d2ae8d1ec4ad20b4ac548bf8db0a34f929320e
Vignesh Chandrasekharan <vchan444@gmail.com>
2018-10-04 22:44:04 -0500

    qrcode works now

--- tabs/Qrcode.js
+++ tabs/Qrcode.js
@@ -1,63 +1,53 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity} from 'react-native';
-import { Camera, Permissions } from 'expo';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import { Constants, BarCodeScanner, Permissions } from 'expo';
 
-export default class Settings extends Component {
+export default class Qrcode extends Component {
+  state = {
+    hasCameraPermission: null
+  };
 
-    constructor(props) {
-        super(props);
-    }
+  componentDidMount() {
+    this._requestCameraPermission();
+  }
 
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
 
-    state = {
-        hasCameraPermission: null,
-        type: Camera.Constants.Type.back,
-      };
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
 
-      async componentWillMount() {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
-        this.setState({ hasCameraPermission: status === 'granted' });
-      }
-
-    
-    render() {
-        const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
-    }
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ?
+          <Text>Requesting for camera permission</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: 200, width: 200 }}
+            />
+        }
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+});
