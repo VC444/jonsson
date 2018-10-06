6aa4db1ad9d0d710461fa4995d7a9590e99cf632
Vignesh Chandrasekharan <vchan444@gmail.com>
2018-10-05 23:22:34 -0500

    -

--- tabs/Redeem.js
+++ tabs/Redeem.js
@@ -20,11 +20,12 @@ export default class Redeem extends Component {
                 <QRCode
                 content='WITH GREAT POWER, COMES GREAT MOKKAI!'
                 //ecl = 'M'
-                // outerEyeStyle='circle'
-                // innerEyeStyle='diamond'
-                backgroundColor = '#c75b12'
-                //color = '#008542'
-                logo={require('../images/temoc_icon.png')}
+                outerEyeStyle='circle'
+                //innerEyeStyle='diamond' - NOT WORKING
+                //backgroundColor = '#c75b12'
+                color = '#c75b12'
+                //logo={require('../images/temoc_icon.png')}
+                //logoSize='12'
                 />
                 </View>
         );
