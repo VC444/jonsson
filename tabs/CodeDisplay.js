b2b4d21087247fe7c96fa2772345cf95bfa35614
Manu Satyan <manusatyan13@gmail.com>
2018-11-09 00:17:59 -0600

    Added Maps button in EventDetails + QRCode now includes UserID

--- tabs/CodeDisplay.js
+++ tabs/CodeDisplay.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage } from 'react-native';
 import { QRCode } from 'react-native-custom-qr-codes';
 
 
 
 export default class CodeDisplay extends Component {
-
+    
 render() {
+
+  console.log('USER ID from CodeDisplay.js is ' + this.props.navigation.state.params.ourUserID.toString());
   return(
+
+    
       <View>
           <Text>This is the QR Code page</Text>
           <QRCode
-          content = {this.props.navigation.state.params.woosh.toString()}
+          content = {'app,' + this.props.navigation.state.params.ourUserID.toString() + ',' + this.props.navigation.state.params.woosh.toString()}
           //ecl = 'M'
           outerEyeStyle='circle'
-          //innerEyeStyle='diamond' - NOT WORKING
+          //innerEyeStyle='diamond'
           //backgroundColor = '#c75b12'
           color = '#c75b12'
           //logo={require('../images/temoc_icon.png')}
