a2ae248ee50373cdc26ca6279120fde444addf7a
Manu Satyan <manusatyan13@gmail.com>
2018-11-16 12:46:08 -0600

    Numerous UI changes

--- tabs/Redeem.js
+++ tabs/Redeem.js
@@ -4,7 +4,7 @@
  */
 
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Button, TouchableOpacity, Alert, AsyncStorage, ImageBackground } from 'react-native';
+import { View, StyleSheet, ScrollView, Button, TouchableOpacity, Alert, AsyncStorage, ImageBackground, Image } from 'react-native';
 import { Container, Header, Content, Accordion, Form, Item, Input, Label, Card, CardItem, Body, Icon, Text } from "native-base";
 import CodeDisplay from './CodeDisplay';
 
@@ -76,9 +76,9 @@ export default class Redeem extends Component {
                 </View>
                 <View style={styles.infoStyle}>
                     <Text style = {styles.bodyStyle}>1. Enter the number of whoosh bits you wish to redeem in the text box below.</Text>
-                    <Text style = {styles.bodyStyle}>2. Then, tap on the "Redeem Whoosh Bits" button below to generate a personal QR code.</Text>
-                    <Text style = {styles.bodyStyle}>3. Now, show this QR code to an attendant to approve your redeem points request!</Text>
-                    <Text style = {styles.bodyStyle}>4. Thanks for attending!</Text>
+                    <Text style = {styles.bodyStyle}>2. Then, tap on the Whoosh Bits icon below to generate your personal QR code!</Text>
+                    <Text style = {styles.bodyStyle}>3. And you're done! That's it!</Text>
+                    <Text style = {styles.bodyStyle}>4. Just show this QR code to an attendant to approve your redeem points request!</Text>
                 </View>
                 
                 <Form style={styles.formView}>
@@ -90,7 +90,12 @@ export default class Redeem extends Component {
                         onPress={() => this.displayQRCode()}
                         style={styles.button}
                     >
-                        <Text style={styles.buttonText}>{this.state.isLoading ? 'Redeeming...' : 'Redeem Whoosh Bits!'}</Text>
+                    <Image
+                    source={require('../images/wbicon.png')}
+                    fadeDuration={0}
+                    style={{width: 120, height: 120, alignItems: 'center'}}
+                    />
+                        {/* <Text style={styles.buttonText}>{this.state.isLoading ? 'Redeeming...' : 'Tap to Redeem!'}</Text> */}
                     </TouchableOpacity>
                 </Form>
             </ScrollView>
