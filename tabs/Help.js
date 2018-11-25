e3496b0847435eccd0ad2ca2984dfa979d1ba193
Manu Satyan <manusatyan13@gmail.com>
2018-11-25 14:44:31 -0600

    Email id for feedback is now added automatically (Fixed)

--- tabs/Help.js
+++ tabs/Help.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, ScrollView, Button, TouchableOpacity, Alert } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Button, TouchableOpacity, Alert, AsyncStorage } from 'react-native';
 import { Container, Header, Content, Accordion, Form, Item, Input, Label } from "native-base";
 import * as firebase from 'firebase';
 import Axios from 'axios';
@@ -10,112 +10,121 @@ export default class Help extends Component {
         super(props);
         this.state = {
             giveFeedback: false,
-            email : '',
-            message : '',
-            isLoading : false,
-            dataArray : []
+            email: '',
+            message: '',
+            isLoading: false,
+            dataArray: [],
+            userEmail: '',
         };
         this.feedbackSubmitted = this.feedbackSubmitted.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleMessageChange = this.handleMessageChange.bind(this);
         this.postFeedback = this.postFeedback.bind(this);
-    this.readUserData = this.readUserData.bind(this);
+        this.readUserData = this.readUserData.bind(this);
     }
 
     readUserData() {
         //console.log('entering')
-        var arr = [] ;
+        var arr = [];
         var self = this;
         fetch('https://jonssonconnect.firebaseio.com/FAQ.json?auth=qKeO1eylfWgscqfLFVGMv298g6KlHo0BRN8E2kFb')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        
-        Object.keys(responseJson).forEach(function (key) {
-        var data = responseJson[key];
-        arr.push({ "title": data.Question, "content": data.Answer });
-        })
+            .then((response) => response.json())
+            .then((responseJson) => {
 
-        //console.log(responseJson)
-        //console.log(arr)
-        self.setState({
-        dataArray : arr
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-        
-      });
-      
+                Object.keys(responseJson).forEach(function (key) {
+                    var data = responseJson[key];
+                    arr.push({ "title": data.Question, "content": data.Answer });
+                })
+
+                //console.log(responseJson)
+                //console.log(arr)
+                self.setState({
+                    dataArray: arr
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+
+            });
+
     }
 
-    handleEmailChange(e){
+    handleEmailChange(e) {
         this.setState({
-            email : e
+            userEmail: e
         });
     }
-    handleMessageChange(e){
+    handleMessageChange(e) {
         this.setState({
-            message : e
+            message: e
         })
     }
-    /**************/
-    postFeedback = (email, message, emailClear, messageClear) =>{
-        if(this.state.message!=null){
-            if(!this.state.isLoading){
-                const self = this;
-            fetch('https://jonssonconnect.firebaseio.com/Feedback.json?auth=qKeO1eylfWgscqfLFVGMv298g6KlHo0BRN8E2kFb', {
-                method: 'POST',
-                headers:{
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json; charset=utf-8',
-                },
-                body: JSON.stringify({
-                    "email": email, 
-                    "message": message,
-                })
-            })
-            .then((response) => response.json() )
-            .then((responseData) => {
-                if(!responseData.error){
-                    self.setState({
-                        email: '', message: '',
-                        isSubmited: true,
-                        giveFeedback : false,
-                        isLoading : false 
-                    })
-                }
-                else {
-                    Alert.alert(
-                        responseData.error,
-                        [
-                            {text: 'OK', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
 
-                        ],
-                        { cancelable: false}
-                    )
-                }
-            })
-            .catch((err)=>{
-                
-            })
-             
-        }
+    async componentWillMount() {
+        this.setState({
+            userEmail: await AsyncStorage.getItem('email'),
+        });
+        console.log("ASYNC EMAIL ID: " + this.state.userEmail)
+    }
+
+    /**************/
+    postFeedback = (email, message, emailClear, messageClear) => {
+        if (this.state.message != null) {
+            if (!this.state.isLoading) {
+                const self = this;
+                fetch('https://jonssonconnect.firebaseio.com/Feedback.json?auth=qKeO1eylfWgscqfLFVGMv298g6KlHo0BRN8E2kFb', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json; charset=utf-8',
+                    },
+                    body: JSON.stringify({
+                        "email": email,
+                        "message": message,
+                    })
+                })
+                    .then((response) => response.json())
+                    .then((responseData) => {
+                        if (!responseData.error) {
+                            self.setState({
+                                email: '', message: '',
+                                isSubmited: true,
+                                giveFeedback: false,
+                                isLoading: false
+                            })
+                        }
+                        else {
+                            Alert.alert(
+                                responseData.error,
+                                [
+                                    { text: 'OK', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+
+                                ],
+                                { cancelable: false }
+                            )
+                        }
+                    })
+                    .catch((err) => {
+
+                    })
+
+            }
         }
         else {
             Alert.alert(
                 'Oops!',
                 'Press the SUBMIT button after entering your message.',
                 [
-                    {text: 'OK', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    { text: 'OK', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
                 ],
-                {cancelable: false}
+                { cancelable: false }
             )
         }
     };
 
     hasWhitespace(str) {
         return (!str || str.length === 0 || /^\s*$/.test(str))
-      }
+    }
 
 
     giveFeedbackPressed = () => {
@@ -124,52 +133,57 @@ export default class Help extends Component {
 
     feedbackSubmitted = () => {
 
-        if (this.hasWhitespace(this.state.email) || this.state.email === null)
-        {
-            Alert.alert(
-                'Oops!',
-                'We noticed that the email field is blank. \n\nDid you tap the feedback button by mistake?',
-                [
-                    {text: 'LOL, Yea!', onPress: () => console.log('blankFeedbackEmail acknowledged!')},
-                ],
-                {cancelable: false}
-            )
-        }
-        else if (this.hasWhitespace(this.state.message) || this.state.message === null)
-        {
+        // if (this.hasWhitespace(this.state.email) || this.state.email === null)
+        // {
+        //     Alert.alert(
+        //         'Oops!',
+        //         'We noticed that the email field is blank. \n\nDid you tap the feedback button by mistake?',
+        //         [
+        //             {text: 'LOL, Yea!', onPress: () => console.log('blankFeedbackEmail acknowledged!')},
+        //         ],
+        //         {cancelable: false}
+        //     )
+        // }
+        if (this.hasWhitespace(this.state.message) || this.state.message === null) {
             Alert.alert(
                 'Oops!',
                 'We noticed that the message field is blank. \n\nDid you tap the feedback button by mistake?',
                 [
-                    {text: 'LOL, Yea!', onPress: () => console.log('blankFeedbackMessage acknowledged!')},
+                    { text: 'LOL, Yea!', onPress: () => console.log('blankFeedbackMessage acknowledged!') },
                 ],
-                {cancelable: false}
+                { cancelable: false }
             )
         }
         else {
             this.setState({
-                isLoading :true
+                isLoading: true
             })
 
             Alert.alert("Feedback has been submitted! Thank you");
             this.setState({
-                isLoading :true
+                isLoading: true
             })
-            this.postFeedback(this.state.email, this.state.message);
+            this.postFeedback(this.state.userEmail, this.state.message);
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.readUserData();
-        }
+    }
 
     render() {
+
         if (this.state.giveFeedback == false) {
+            console.log("USER EMAIL ID: " + this.state.userEmail)
             return (
                 <ScrollView style={styles.masterView}>
+                <Text style={{ paddingHorizontal: 20, textAlign: 'left', paddingVertical: 25, fontWeight: 'bold' }}>
+            Below, you will find some of the most Frequently Asked Questions (hence, FAQ) and their answers.
+            If you don't find the answers you are looking for, scroll down to the bottom and fill out the feedback form 
+            and we'll get back to you ASAP.</Text>
                     <View>
                         <Accordion
-                            
+
                             dataArray={this.state.dataArray}
                             headerStyle={{ backgroundColor: "#FFFFFF" }}
                         />
@@ -186,8 +200,13 @@ export default class Help extends Component {
                 </ScrollView>
             );
         } else {
+            console.log("USER EMAIL ID: " + this.state.userEmail)
             return (
                 <ScrollView style={styles.masterView}>
+                <Text style={{ paddingHorizontal: 20, textAlign: 'left', paddingVertical: 25, fontWeight: 'bold' }}>
+            Below, you will find some of the most Frequently Asked Questions (hence, FAQ) and their answers.
+            If you don't find the answers you are looking for, scroll down to the bottom and fill out the feedback form 
+            and we'll get back to you ASAP.</Text>
                     <View>
                         <Accordion
                             dataArray={this.state.dataArray}
@@ -196,21 +215,25 @@ export default class Help extends Component {
                     </View>
 
                     <Form style={styles.formView}>
-                        <Item stackedLabel>
+                        {/* <Item stackedLabel>
                             <Label>Email ID (@utdallas.edu is preferred!)</Label>
                             <Input value={this.state.email} onChangeText={(e)=>{this.handleEmailChange(e)}} name="email"/>
-                        </Item>
+                        </Item> */}
                         <Item stackedLabel>
                             <Label>How can we improve?</Label>
-                            <Input value={this.state.message} onChangeText={(e)=>{this.handleMessageChange(e)}} name="message"/>
+                            <Input value={this.state.message} onChangeText={(e) => { this.handleMessageChange(e) }} name="message" />
                         </Item>
-                        
+
                         <TouchableOpacity
                             onPress={this.feedbackSubmitted}
                             style={styles.button}
                         >
-                            <Text style={styles.buttonText}>{this.state.isLoading?'Submitting... Give Us A Sec!':'Submit'}</Text>
+                            <Text style={styles.buttonText}>{this.state.isLoading ? 'Submitting... Give Us A Sec!' : 'Submit'}</Text>
                         </TouchableOpacity>
+
+                        <Text style={{ paddingHorizontal: 40, textAlign: 'center', paddingVertical: 25 }}>
+                            The email ID that you used to sign into the app will be sent along with your feedback.
+                        This will help us reach out to you if we need to after hearing from you!</Text>
 
                     </Form>
 
