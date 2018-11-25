c5ea252153eaf4a1094918fdeaf387b828a406b8
Manu Satyan <manusatyan13@gmail.com>
2018-11-25 16:47:10 -0600

    Incremental UI updates

--- tabs/ArticleDetails.js
+++ tabs/ArticleDetails.js
@@ -48,7 +48,7 @@
    render() {
      if (this.state.isLoading) {
        return (
-         <View style={{flex: 1, paddingTop: 20}}>
+         <View style={{flex: 1}}>
            <ActivityIndicator />
          </View>
        );
@@ -56,22 +56,19 @@
      return (
        <Container>
        <Content>
-       <Image source={{uri: this.props.navigation.state.params.rowData.articleImageURL}} style={{ height: 200, width: null }}></Image>
-       <Card style={{flex: 0}}>
+       <Image source={{uri: this.props.navigation.state.params.rowData.articleImageURL}} style={{ height: 300 }}></Image>
+       <Card>
            <CardItem>
              <Body>
                <Text style={styles.articlenameStyle}>{this.props.navigation.state.params.rowData.articleName}</Text>
                <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
                <Text style={{fontSize: 14, color: '#808080',}}><Icon name='ios-clock-outline' style={{ fontSize: 14}}/> Published on {new Date(this.props.navigation.state.params.rowData.postedOn).toLocaleDateString("en-US", { month:'short', day:'numeric', year:'numeric' })}</Text>
                <Text style={{fontSize: 12, fontWeight: '100', color: "#b6b6b6",}}>_____________________________________________________________</Text>
-               <Text style={{fontSize: 12, fontWeight: '800'}}></Text>
-               <Text style={{fontSize: 14, fontWeight: '800'}}> <Icon name='ios-paper-outline' style={{ fontSize: 14}}/> Article Highlights</Text>
-               <Text style={{fontSize: 12, fontWeight: '100'}}></Text>
-               <Text style={styles.contentStyle}>{this.props.navigation.state.params.rowData.articleContent}</Text>
+               <Text style={styles.contentStyle}>{'\n\t\t\t\t\t' + this.props.navigation.state.params.rowData.articleContent + '\n\n'}</Text>
+               <Text style={{fontSize: 14, fontWeight: '800', alignSelf: 'center'}}>Thanks For Reading! {'\t'} :) {'\n\n'}</Text>
+               <Text style={{fontSize: 10, fontWeight: '100', color:'#b6b6b6', alignSelf: 'center'}}> --- End of News Article ---</Text>
+               
              </Body>
-           </CardItem>
-           <CardItem>
-           <Image source={{uri: 'http://ecs.utdallas.edu/aseegsw17/images/jonsson-school-logo.jpg'}} style={{ height: 200, width: null }}></Image>
            </CardItem>
          </Card>
        </Content>
--- tabs/JobsDetails.js
+++ tabs/JobsDetails.js
@@ -3,30 +3,30 @@
  * https://github.com/mendoza-git/JonssonConnect
  * @flow
  */
- import React, { Component } from 'react';
- import { ActivityIndicator, Image, ListView, Linking, FlatList, StyleSheet, View, ImageBackground } from 'react-native';
- import { createBottomTabNavigator, createStackNavigator } from "react-navigation";
- import { Container, Header, Content, Card, Col, CardItem, Grid, Thumbnail, List, ListItem, Icon, Item, Input, Text, Title, Button, Left, Body, Right, Row, H1, H2, H3 } from 'native-base';
- import * as firebase from 'firebase';
+import React, { Component } from 'react';
+import { ActivityIndicator, Image, ListView, Linking, FlatList, StyleSheet, View, ImageBackground } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from "react-navigation";
+import { Container, Header, Content, Card, Col, CardItem, Grid, Thumbnail, List, ListItem, Icon, Item, Input, Text, Title, Button, Left, Body, Right, Row, H1, H2, H3 } from 'native-base';
+import * as firebase from 'firebase';
 
- export default class JobsDetails extends Component {
+export default class JobsDetails extends Component {
 
-   constructor(props) {
-     super(props);
-     this.state = {
-       isLoading: true
-     }
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
 
-   componentDidMount() {
+  componentDidMount() {
     return fetch('https://jonssonconnect.firebaseio.com/Jobs.json')
       .then((response) => response.json())
       .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.setState({
           isLoading: false,
           dataSource: ds.cloneWithRows(responseJson),
-        }, function() {
+        }, function () {
           // do something with new state
         });
       })
@@ -35,85 +35,88 @@
       });
   }
 
-   static navigationOptions = {
-     tabBarLabel: 'Jobs',
-     tabBarIcon: ({ tintcolor }) => (
-       <Image
+  static navigationOptions = {
+    tabBarLabel: 'Jobs',
+    tabBarIcon: ({ tintcolor }) => (
+      <Image
         source={require('../images/briefcaseicon.png')}
-        style={{width: 22, height: 22}}>
-       </Image>
-     )
-   }
+        style={{ width: 22, height: 22 }}>
+      </Image>
+    )
+  }
 
-   render() {
-     if (this.state.isLoading) {
-       return (
-         <View style={{flex: 1, paddingTop: 20}}>
-           <ActivityIndicator />
-         </View>
-       );
-     }
-     return (
-       <Container style={styles.containerStyle}>
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      <Container style={styles.containerStyle}>
         <Content>
           <View style={styles.container2}>
             <ImageBackground
               style={styles.backdrop}
               blurRadius={5}
-              source={{uri: this.props.navigation.state.params.rowData.backgroundURL}}>
-                <View style={styles.backdropView}>
-                  <Thumbnail square source={{uri: this.props.navigation.state.params.rowData.companyImageURL}} />
-                  <Text style={styles.positionheadline}>{this.props.navigation.state.params.rowData.positionTitle}</Text>
-                  <Text style={styles.companyheadline}>{this.props.navigation.state.params.rowData.companyName}</Text>
-                  <Text style={styles.locationheadline}>{this.props.navigation.state.params.rowData.jobLocation}</Text>
-                </View>
+              source={{ uri: this.props.navigation.state.params.rowData.backgroundURL }}>
+              <View style={styles.backdropView}>
+                <Thumbnail square source={{ uri: this.props.navigation.state.params.rowData.companyImageURL }} />
+                <Text style={styles.positionheadline}>{this.props.navigation.state.params.rowData.positionTitle}</Text>
+                <Text style={styles.companyheadline}>{this.props.navigation.state.params.rowData.companyName}</Text>
+                <Text style={styles.locationheadline}>{this.props.navigation.state.params.rowData.jobLocation}</Text>
+              </View>
             </ImageBackground>
           </View>
-          <Card style={{ backgroundColor: '#f8f6f6'}}>
-            <Button full style={styles.buttonStyle} onPress={ ()=>{ Linking.openURL(this.props.navigation.state.params.rowData.applicationURL)}}>
-              <Text style={{ fontSize: 14, fontWeight: '500'}}><Icon name='ios-link' style={{ fontSize: 14, color: '#ffffff'}}/> Apply Now</Text>
+          <Card style={{ backgroundColor: '#f8f6f6' }}>
+            <Button full style={styles.buttonStyle} onPress={() => { Linking.openURL(this.props.navigation.state.params.rowData.applicationURL) }}>
+              <Text style={{ fontSize: 14, fontWeight: '500' }}><Icon name='ios-link' style={{ fontSize: 14, color: '#ffffff' }} /> Apply Now</Text>
             </Button>
           </Card>
-          <Text style={{fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10}}><Icon name='ios-flash' style={{ fontSize: 14}}/> Position Overview</Text>
+          <Text style={{ fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10 }}><Icon name='ios-flash' style={{ fontSize: 14 }} /> Position Overview</Text>
           <Text style={styles.descriptionStyle}>{this.props.navigation.state.params.rowData.positionOverview}</Text>
-          <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
-          <Text style={{fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10}}><Icon name='ios-stats' style={{ fontSize: 14}}/> Qualifications</Text>
+          <Text style={{ fontSize: 14, fontWeight: '800' }}></Text>
+          <Text style={{ fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10 }}><Icon name='ios-stats' style={{ fontSize: 14 }} /> Qualifications</Text>
           <Text style={styles.descriptionStyle}>{this.props.navigation.state.params.rowData.positionQualifications}</Text>
-          <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
-          <Text style={{fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10}}><Icon name='ios-school' style={{ fontSize: 14}}/> Desired Majors</Text>
-          <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
+          <Text style={{ fontSize: 14, fontWeight: '800' }}></Text>
+          <Text style={{ fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10 }}><Icon name='ios-school' style={{ fontSize: 14 }} /> Desired Majors</Text>
+          <Text style={{ fontSize: 14, fontWeight: '800' }}></Text>
           <Text style={styles.descriptionStyle}>{this.props.navigation.state.params.rowData.desiredMajors}</Text>
-          <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
-          <Text style={{fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10}}><Icon name='ios-alarm' style={{ fontSize: 14}}/> Job-Type</Text>
-          <Text style={{fontSize: 14, fontWeight: '800'}}></Text>
-          <Text style={styles.descriptionStyle}>{this.props.navigation.state.params.rowData.jobType}</Text>
-        </Content>
-       </Container>
-     )
-   }
- }
+          <Text style={{ fontSize: 14, fontWeight: '800' }}></Text>
+          <Text style={{ fontSize: 14, fontWeight: '800', paddingLeft: 15, paddingTop: 10 }}><Icon type='MaterialCommunityIcons' name='worker' style={{ fontSize: 14 }} /> Job-Type</Text>
+          <Text style={{ fontSize: 14, fontWeight: '800' }}></Text>
+          <Text style={styles.descriptionStyle}>{this.props.navigation.state.params.rowData.jobType + '\n\n'}</Text>
+          <Text style={{ fontSize: 14, fontWeight: '800', alignSelf: 'center' }}>Check Us Out On Handshake! {'\t'} :) {'\n\n'}</Text>
+          <Text style={{ fontSize: 10, fontWeight: '100', color: '#b6b6b6', alignSelf: 'center' }}> --- End of Job Details --- {'\n'}</Text>
 
- const styles = StyleSheet.create({
+        </Content>
+      </Container>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
   containerStyle: {
-     backgroundColor: '#FFFFFF',
+    backgroundColor: '#FFFFFF',
   },
   applicationStyle: {
-      fontWeight: '600',
-      fontSize: 12,
+    fontWeight: '600',
+    fontSize: 12,
   },
   buttonStyle: {
-     backgroundColor: '#5BC6E8',
-     height: 40,
+    backgroundColor: '#5BC6E8',
+    height: 40,
   },
   nameStyle: {
-     fontWeight: '600',
-     fontSize: 16,
+    fontWeight: '600',
+    fontSize: 16,
   },
   descriptionStyle: {
-     fontWeight: '100',
-     fontSize: 12,
-     paddingLeft: 15,
-     paddingTop: 10,
+    fontWeight: '100',
+    fontSize: 12,
+    paddingLeft: 15,
+    paddingTop: 10,
   },
   hostStyle: {
     fontSize: 12,
