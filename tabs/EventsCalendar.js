74150a24f8f7148c441dd5116813c95fb7fc51a9
Manu Satyan <manusatyan13@gmail.com>
2018-12-05 19:01:20 -0600

    Fixed Calendar issue +  whoosh bits check

--- tabs/EventsCalendar.js
+++ tabs/EventsCalendar.js
@@ -281,11 +281,11 @@ export default class EventsCalendar extends Component {
         const date = new Date()
         var year = date.getFullYear()
         var month = date.getMonth() + 1;
-        // if (month <= 9)
-        //     month = '0' + month;
+        if (month <= 9)
+            month = '0' + month;
         var day = date.getDate();
-        // if (day <= 9)
-        //     day = '0' + day;
+        if (day <= 9)
+            day = '0' + day;
         var fullDate = year + '-' + month + '-' + day;
         var stringDate = fullDate.toString();
         console.log('this is fulldateeeeeee' + stringDate);
